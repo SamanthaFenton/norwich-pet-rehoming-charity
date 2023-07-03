c743546f0229f0fe11f061d4f885dea04c56ff16
Samantha <samantha.fenton@live.co.uk>
2023-07-03 13:20:56 +0100

    welcome message in js asking name and instruction to choose theme set, js and css to enter light and dark mode on index set

--- js/1-home.js
+++ js/1-home.js
@@ -1,2 +1,11 @@
 "use strict";
 console.log("sheet-one");
+
+const welcome = alert("Welcome to Norpet, Norwich's specialist local animal adoption and rehoming charity");
+console.log(welcome);
+
+let user = prompt("Please tell me your name?").toLowerCase();
+console.log(user);
+
+const greet = alert("Thanks " + user + " , please personalise your experience by selecting a theme for the website.");
+console.log(greet);
